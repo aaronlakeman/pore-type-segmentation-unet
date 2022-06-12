94d83e3872f5f3a9ee41c42f8fdd00b7c25162ed
Max Hallenberger <guuzfraaba@gmail.com>
2022-06-12 14:33:25 +0200

    predict on test image works now

--- modeling/baseline_model.py
+++ modeling/baseline_model.py
@@ -1,18 +1,24 @@
-import glob
 import cv2
 import numpy as np
 
 
-def baseline_model(corr_masks, uncorrected_masks):
-    list = []
-    for correct in glob.glob(corr_masks + "*.tif"):
-        for uncorrect in glob.glob(uncorrected_masks + "*.tif"):
-            img_1 = cv2.imread(correct, 1)
-            img_2 = cv2.imread(uncorrect, 1)
+def baseline_model(corr_masks, uncorrected_masks, img_list, msk_list):
+    """Calculates the accuracy based on the pixel-vice difference between images
 
-            res = cv2.absdiff(img_1, img_2)
-            res = res.astype(np.uint8)
-            percentage = np.round(100 - ((np.count_nonzero(res) * 100) / res.size), 2)
-            list.append(percentage)
-            print(f"The mean accuracy is equal to {np.mean(list)}")
-            print(f"These are the accuracies of the individual images{list}")
+    Args:
+        corr_masks (_type_): _description_
+        uncorrected_masks (_type_): _description_
+        img_list (_type_): _description_
+        msk_list (_type_): _description_
+    """
+    list = []
+    for i in range(len(img_list)):
+        img_1 = cv2.imread(corr_masks + img_list[i], 1)
+        img_2 = cv2.imread(uncorrected_masks + msk_list[i], 1)
+
+        res = cv2.absdiff(img_1, img_2)
+        res = res.astype(np.uint8)
+        percentage = np.round(100 - ((np.count_nonzero(res) * 100) / res.size), 2)
+        list.append(percentage)
+    print(f"The mean accuracy is equal to {round(np.mean(list), 2)}")
+    print(f"These are the accuracies of the individual images{list}")
